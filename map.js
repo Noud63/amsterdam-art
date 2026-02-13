57b8294a05260b19b83cbdb8cc5f0fa4605641db
Noud63 <noudvandun@gmail.com>
2026-02-13 13:18:09 +0100

    update

--- map.js
+++ map.js
@@ -4,22 +4,27 @@
 // 	}).setView([52.371534, 4.862805], 14);
 // mymap.zoomControl.setPosition('bottomright');		
 		
-// L.tileLayer('https://api.mapbox.com/styles/v1/noud/cjv7mpqox0hps1fs1tzwk9fgx/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoibm91ZCIsImEiOiJjanYyY291OHgxMGh3NGVvazlneWV0MnhsIn0.qQ8Thc6rxi03CybmIomvmQ', 
+// L.tileLayer('https://api.mapbox.com/styles/v1/noud/cmlku6k95002601sf8p344580/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoibm91ZCIsImEiOiJjanYyY291OHgxMGh3NGVvazlneWV0MnhsIn0.qQ8Thc6rxi03CybmIomvmQ&v=', 
 // 		{
 //     tileSize: 512,
 //     zoomOffset: -1,
 //     maxZoom: 18,
+// 	detectRetina: true,
 //     attribution: '© Mapbox © OpenStreetMap'
 //   }
         
 // ).addTo(mymap);
 
 
+// Styles
+// /noud/cjv7mpqox0hps1fs1tzwk9fgx
+// /noud/cmlku6k95002601sf8p344580
+// /noud/cmlkufksc007201r81z3s3vtu
 
 // Tiles styles:
-//'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}' (Esri)
-//'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}' (default)
-//mapbox://styles/noud/cjv7mpqox0hps1fs1tzwk9fgx
+// 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}' (Esri)
+// 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}' (default)
+// mapbox://styles/noud/cjv7mpqox0hps1fs1tzwk9fgx
 
 const mymap = L.map('mapid', {
     zoomControl: true
